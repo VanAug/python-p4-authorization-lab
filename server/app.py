6e477a0c18058208098e4e0725af62837824468e
Brian <okellobrian18@gmail.com>
2025-06-21 11:34:24 +0300

    authorizing lab

--- server/app.py
+++ server/app.py
@@ -85,14 +85,26 @@ class CheckSession(Resource):
         return {}, 401
 
 class MemberOnlyIndex(Resource):
-    
     def get(self):
-        pass
+        if not session.get("user_id"):
+            return {"error": "Unauthorized access. Please log in."}, 401
+
+        articles = [article.to_dict() for article in Article.query.filter_by(is_member_only=True).all()]
+        return articles, 200
+
 
 class MemberOnlyArticle(Resource):
-    
     def get(self, id):
-        pass
+        if not session.get("user_id"):
+            return {"error": "Unauthorized access. Please log in."}, 401
+
+        article = Article.query.filter_by(id=id, is_member_only=True).first()
+
+        if not article:
+            return {"error": "Article not found or not available to non-members"}, 404
+
+        return article.to_dict(), 200
+
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(IndexArticle, '/articles', endpoint='article_list')
